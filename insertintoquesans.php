e57daa34b7b72b199d73c3f92d71f7730ef78893
Raha10 <soumya.raha@innoraft.com>
2017-04-06 12:50:53 +0530

    changes made as the insert questions part is added

--- insertintoquesans.php
+++ insertintoquesans.php
@@ -1,40 +1,44 @@
 <?php
-$connection = mysql_connect("localhost", "root", "9433"); // Establishing Connection with Server
-$db = mysql_select_db("quizdb", $connection); // Selecting Database from Server
+$conn= mysql_connect("localhost", "root", "9433"); // Establishing Connection with Server
+$db = mysql_select_db("quizdb", $conn);
+// Selecting Database from Server
 if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+	$cat=$_POST['category'];
+	$catquery= mysql_query("insert into categories(c_name)values('".$cat."')");
+$sql=mysql_query("select c_id from categories where c_name='".$cat."'");
+// $rs=mysql_query($conn,$sql);
+// $row=mysql_fetch_assoc($rs);
+// 	$last_id=$row;
+	// if(mysqli_query($conn,$catquery))
+	// {
+		// $last_id = mysqli_insert_id($conn);
+	// }
+// }
 
-$cat = $_POST['category'];
+$result = mysql_fetch_assoc($sql);
+$result_cid=$result['c_id'];
 
-	$query=mysql_query("insert into categories(c_name)  values ('$cat')");
 
 $question = $_POST['question'];
 $opt1 = $_POST['option1'];
-$opt2=($_POST['option2']);
-$opt3=($_POST['option3']);
-$opt4=($_POST['option4']);
-$correct_ans=($_POST['correct_ans']);
-$sql=mysql_query("select c_id from categories where c_name='$cat'");
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $c_id=$result;
-    }
-} else {
-    echo "0 results";
-}
+$opt2=$_POST['option2'];
+$opt3=$_POST['option3'];
+$opt4=$_POST['option4'];
+$corr_ans=$_POST['correct_answer'];
 
-
-
-if($question !=''||$opt1 !=''||$opt1 !=''||$opt2 !=''||$opt3 !=''||$opt4 !=''||$correct_ans !=''){
+if($question !=''||$opt1 !=''||$opt2 !=''||$opt3 !=''||$opt4 !=''||$corr_ans !=''){
 //Insert Query of SQL
-$query = mysql_query("insert into quesans(q,opt1,opt2,opt3,opt4,ca,c_id)  values ('$question','$opt1','$opt2','$opt3','$opt4','$correct_ans',$c_id)");
+$query = mysql_query("insert into quesans(q,opt1,opt2,opt3,opt4,ca,c_id)  values ('".$question."','".$opt1."','".$opt2."','".$opt3."','".$opt4."','".$corr_ans."','".$result_cid."')");
 echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+
 }
 else{
+
 echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
 }
 }
 
-mysql_close($connection); // Closing Connection with Server
+mysql_close($conn);
+
+// Closing Connection with Server
 ?>
